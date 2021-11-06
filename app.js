37f3cbbb79dc24a8cd0aa02d1c4bf70d33ce7593
Nguyen Duc Trong <trongnguyen0324.ggt@gmail.com>
2021-11-06 18:37:01 +0700

    update port in env

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-var cookieParser = require('cookie-parser')
-var morgan = require('morgan')
-
+var cookieParser = require('cookie-parser');
+var morgan = require('morgan');
 
 // Define Router
 const authRouter = require('./routers/auth.router');
@@ -18,16 +17,18 @@ const userName = require('./middlewares/username.middleware');
 const authMiddleware = require('./middlewares/auth.middleware');
 // const authorizationMiddleware = require('./middlewares/authorization.middleware');
 
-
 const app = express();
 
 // getting-started.js
-mongoose.connect(`mongodb+srv://ecommer:${process.env.PASSWORD}@ecommer.w6zqa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, { useNewUrlParser: true });
+mongoose.connect(
+    `mongodb+srv://ecommer:${process.env.PASSWORD}@ecommer.w6zqa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-  // we're connected!
-  console.log('Connect success!')
+    // we're connected!
+    console.log('Connect success!');
 });
 
 // Config
@@ -37,12 +38,12 @@ app.use(express.static('public'));
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 // Middleware use for all router
 app.use(userName.userName);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/', productRouter);
 app.use('/search', searchRouter);
@@ -51,7 +52,9 @@ app.use('/user', authMiddleware.loginMiddleware, userRouter);
 app.use('/seller', authMiddleware.loginMiddleware, sellerRouter);
 app.use('/api', authMiddleware.loginMiddleware, apiRouter);
 
-app.listen(port, () => console.log(`Server listen at http://localhost:${port}`));
+app.listen(port, () =>
+    console.log(`Server listen at http://localhost:${port}`)
+);
 
 // todo: find product, bill, purcharse
 // todo: filter bill.....
